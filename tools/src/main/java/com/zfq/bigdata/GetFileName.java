7045cf3d74489ed8aacb5285d8d002b9638c222c
zfq314 <zfq0314@163.com>
2024-11-28 17:56:33 +0800

    modify dir

--- tools/src/main/java/com/zfq/bigdata/GetFileName.java
+++ tools/src/main/java/com/zfq/bigdata/GetFileName.java
@@ -14,7 +14,7 @@ public class GetFileName {
         //获取文件路径文件夹下的全部文件列表
         System.out.println("文件有如下：");
         //表示一个文件路径
-        File file = new File("./");
+        File file = new File("D:\\桌面\\数据还原\\OK");
         //用数组把文件夹下的文件存起来
         File[] files = file.listFiles();
         //foreach遍历数组
@@ -22,7 +22,7 @@ public class GetFileName {
             //打印文件列表：只读取名称使用getName();
             //System.out.println("路径："+file2.getPath());
             //
-            System.out.println(""+file2.getName());
+            System.out.println("/data/program/datax/bin/datax.py /root/hy/"+file2.getName());
         }
 
     }
